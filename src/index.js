1ac0587a7fec4352da136dd621019439393be56c
kaifsaif <kaifkhan.biz@gmail.com>
2023-10-01 17:29:55 +0530

    Added index.js in src folder & update package.json file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-const server = require('./routers/app')
+const server = require('../routers/app')
 
 const PORT = process.env.PORT || 3000
+
+require('dotenv').config()
 
 server.listen(PORT,()=>{
     console.log(`Listening at ${PORT}`);
